6b6594aa57ce21a2bfabc73159f36071b9cea5b9
mahesh-ltts <60654633+mahesh-ltts@users.noreply.github.com>
2022-07-07 22:39:02 +0530

    props

--- src/daily-concept-wise/class.compoennt.js
+++ src/daily-concept-wise/class.compoennt.js
@@ -1,28 +1,68 @@
 import { Component } from "react"
 import "./styles.css";
 
+
 class ClassComponent extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-         this.state = {
-            name: "Hello",
-            isStudent: true
-         }
+        this.state = {
+            name: "Mosesh",
+            student: "B.tech"
+        }
     }
     // inline/, internal, external
-    render(){
+    render() {
         const stleFormate = {
             color: "red",
         }
         return (
             <div>
                 <h1 style={stleFormate} >{this.state.name}</h1>
-                <h1 class="app">{this.state.isStudent? "Boys" : "girls" }</h1>
-                <h1 id="id">{this.state.isStudent? "Boys" : "girls" }</h1>
+                <h1 class="app">{this.state.isStudent ? "Boys" : "girls"}</h1>
+                <ClassChild  pass={this.state.name} passOne={122345678}/>
+                <ClassChildTwo passTwo={this.state.student}/>
             </div>
+        )
+    }
+}
+
+
+class ClassChild extends ClassComponent {
+    constructor(props) {
+        super(props)
+        this.state = {
+            
+        }
+    }
+    render() {
+        const {pass, passOne} = this.props;
+        return (
+            <>
+            <h1>Hello: {pass + " "+ passOne}</h1>
+            </>
         )
     }
 
 }
+
+class ClassChildTwo extends ClassComponent {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: "hello"   
+        }
+    }
+    render() {
+        return (
+            <>
+            <h1>Hello: {this.props.passTwo +" "+ this.state.name}</h1>
+            firstname : <input type="text" name="firstname" /><br/>
+            secondname : <input type="text" name="secondname" />
+            </>
+        )
+    }
+
+}
+
 
 export default ClassComponent; 
