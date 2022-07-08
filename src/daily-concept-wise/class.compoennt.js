260e8f4eb61771a7b9d4a83f4dc21be6b6c14465
mahesh-ltts <60654633+mahesh-ltts@users.noreply.github.com>
2022-07-08 23:47:29 +0530

    setState completed

--- src/daily-concept-wise/class.compoennt.js
+++ src/daily-concept-wise/class.compoennt.js
@@ -10,17 +10,19 @@ class ClassComponent extends Component {
             student: "B.tech"
         }
     }
+
     // inline/, internal, external
     render() {
         const stleFormate = {
             color: "red",
-        }
+        };
         return (
             <div>
                 <h1 style={stleFormate} >{this.state.name}</h1>
-                <h1 class="app">{this.state.isStudent ? "Boys" : "girls"}</h1>
-                <ClassChild  pass={this.state.name} passOne={122345678}/>
-                <ClassChildTwo passTwo={this.state.student}/>
+                <h1 class="app" style={{color: "red"}}>{this.state.isStudent ? "Boys" : "girls"}</h1>
+                <ClassChild pass={this.state.name} passOne={122345678}/>
+                <ClassChildTwo passTwo={this.state.student} />
+                <LifeCycles />
             </div>
         )
     }
@@ -35,10 +37,10 @@ class ClassChild extends ClassComponent {
         }
     }
     render() {
-        const {pass, passOne} = this.props;
+        // const {pass, passOne} = this.props;
         return (
             <>
-            <h1>Hello: {pass + " "+ passOne}</h1>
+            <h1>Hello: {this.props.pass + " "+ this.props.passOne}</h1>
             </>
         )
     }
@@ -58,6 +60,67 @@ class ClassChildTwo extends ClassComponent {
             <h1>Hello: {this.props.passTwo +" "+ this.state.name}</h1>
             firstname : <input type="text" name="firstname" /><br/>
             secondname : <input type="text" name="secondname" />
+            </>
+        )
+    }
+}
+
+class LifeCycles extends ClassComponent {
+    constructor(props) {
+        super(props)
+        this.state = {
+            studentName: "",
+            isShow: false
+        }
+    }
+       // it will reset the data whenever page rerenders
+       componentDidMount(){
+        // code
+        // this.setState((prev)=>({
+        //     ...prev,
+        //     name : "life cycke"
+        // }))
+       };
+       // is invoked immediately before a component is unmounted and destroyed
+       componentWillUnmount(){
+        // code
+       };
+      // componentDidUpdate() is invoked immediately after updating occurs. This method is not called for the initial render.
+       componentDidUpdate(){
+        // code
+       }
+    render() {
+                // function Gree(){
+        //     return new Promise((resolve, reject)=>{
+        //         if(1 ==1){
+        //             resolve("true")
+        //         } else {
+        //             reject("false")
+        //         }
+        //     })
+        // }
+
+        const handleChange = (e) => {
+            // sythetic event
+            // console.log("enterdaat :::",e.target.value);
+            this.setState((prev)=>({
+                ...prev,
+                shbdv: e.target.value
+            }));
+        }
+        const handleClick = () =>{
+            this.setState((prev)=>({
+                ...prev,
+                isShow: true
+            }))
+        }
+        return (
+            <>
+            <h1>Hello: {this.state.name}</h1>
+            name :<input type="text"  placeholder="name" value={this.state.shbdv} onChange={handleChange} />
+            <input type={"button"} value="ok" onClick={handleClick} /><br/>
+            <strong>Entering value : </strong><h1>{this.state.isShow ? this.state.shbdv : ""}</h1>
+            
             </>
         )
     }
