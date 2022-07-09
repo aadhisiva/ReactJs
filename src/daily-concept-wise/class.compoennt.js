de76db8c1a3742b6c25add1a39e2d67ae67ef941
mahesh-ltts <60654633+mahesh-ltts@users.noreply.github.com>
2022-07-09 22:26:10 +0530

    form validation

--- src/daily-concept-wise/class.compoennt.js
+++ src/daily-concept-wise/class.compoennt.js
@@ -19,8 +19,8 @@ class ClassComponent extends Component {
         return (
             <div>
                 <h1 style={stleFormate} >{this.state.name}</h1>
-                <h1 class="app" style={{color: "red"}}>{this.state.isStudent ? "Boys" : "girls"}</h1>
-                <ClassChild pass={this.state.name} passOne={122345678}/>
+                <h1 class="app" style={{ color: "red" }}>{this.state.isStudent ? "Boys" : "girls"}</h1>
+                <ClassChild pass={this.state.name} passOne={122345678} />
                 <ClassChildTwo passTwo={this.state.student} />
                 <LifeCycles />
             </div>
@@ -33,14 +33,14 @@ class ClassChild extends ClassComponent {
     constructor(props) {
         super(props)
         this.state = {
-            
+
         }
     }
     render() {
         // const {pass, passOne} = this.props;
         return (
             <>
-            <h1>Hello: {this.props.pass + " "+ this.props.passOne}</h1>
+                <h1>Hello: {this.props.pass + " " + this.props.passOne}</h1>
             </>
         )
     }
@@ -51,15 +51,15 @@ class ClassChildTwo extends ClassComponent {
     constructor(props) {
         super(props)
         this.state = {
-            name: "hello"   
+            name: "hello"
         }
     }
     render() {
         return (
             <>
-            <h1>Hello: {this.props.passTwo +" "+ this.state.name}</h1>
-            firstname : <input type="text" name="firstname" /><br/>
-            secondname : <input type="text" name="secondname" />
+                <h1>Hello: {this.props.passTwo + " " + this.state.name}</h1>
+                firstname : <input type="text" name="firstname" /><br />
+                secondname : <input type="text" name="secondname" />
             </>
         )
     }
@@ -72,25 +72,42 @@ class LifeCycles extends ClassComponent {
             studentName: "",
             isShow: false
         }
+        // this.handleChange = this.handleChange.bind(this);
     }
-       // it will reset the data whenever page rerenders
-       componentDidMount(){
+    // it will reset the data whenever page rerenders
+    componentDidMount() {
         // code
         // this.setState((prev)=>({
         //     ...prev,
         //     name : "life cycke"
         // }))
-       };
-       // is invoked immediately before a component is unmounted and destroyed
-       componentWillUnmount(){
+    };
+    // is invoked immediately before a component is unmounted and destroyed
+    componentWillUnmount() {
         // code
-       };
-      // componentDidUpdate() is invoked immediately after updating occurs. This method is not called for the initial render.
-       componentDidUpdate(){
+    };
+    // componentDidUpdate() is invoked immediately after updating occurs. This method is not called for the initial render.
+    componentDidUpdate() {
         // code
-       }
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.setState((prev) => ({
+            ...prev,
+            isShow: true
+        }))
+    }
+    handleChange(e) {
+        // sythetic event
+        // console.log("enterdaat :::",e.target.value);
+        this.setState((prev) => ({
+            ...prev,
+            studentName: e.target.value
+        }));
+    }
     render() {
-                // function Gree(){
+        // function Gree(){
         //     return new Promise((resolve, reject)=>{
         //         if(1 ==1){
         //             resolve("true")
@@ -100,27 +117,24 @@ class LifeCycles extends ClassComponent {
         //     })
         // }
 
-        const handleChange = (e) => {
-            // sythetic event
-            // console.log("enterdaat :::",e.target.value);
-            this.setState((prev)=>({
-                ...prev,
-                shbdv: e.target.value
-            }));
-        }
-        const handleClick = () =>{
-            this.setState((prev)=>({
+        const handleClick = () => {
+            this.setState((prev) => ({
                 ...prev,
                 isShow: true
             }))
         }
+        console.log("show ::",this.state.isShow)
         return (
             <>
-            <h1>Hello: {this.state.name}</h1>
-            name :<input type="text"  placeholder="name" value={this.state.shbdv} onChange={handleChange} />
-            <input type={"button"} value="ok" onClick={handleClick} /><br/>
-            <strong>Entering value : </strong><h1>{this.state.isShow ? this.state.shbdv : ""}</h1>
-            
+                {/* <h1>Hello: {this.state.name}</h1>
+            name :<input type="text"  placeholder="name" value={this.state.studentName} onChange={handleChange} />
+            <input type="button" value="ok" onClick={handleClick} /><br/>
+            <strong>Entering value : </strong><h1>{this.state.isShow ? this.state.studentName : ""}</h1> */}
+                <form onSubmit={(e) =>this.handleSubmit(e)}><br />
+                    <input type="text" placeholder="name" value={this.state.studentName} onChange={(e)=>this.handleChange(e)} /><br />
+                    <button type="submit">Submit</button>
+                    <strong>Entering value : </strong><h1>{this.state.isShow ? this.state.studentName : ""}</h1>
+                </form>
             </>
         )
     }
